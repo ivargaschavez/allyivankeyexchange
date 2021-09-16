ef3a8893cc8fe66d483c5eb3fff01838bdef2a13
alison-yu22 <ayu@colgate.edu>
2021-09-15 21:21:46 -0400

    more key changes

--- SecureMessaging.py
+++ SecureMessaging.py
@@ -18,6 +18,7 @@ import pyDH
 
 QUEUE_LENGTH = 1
 SEND_BUFFER_SIZE = 2048
+AES_KEY_SIZE = 32
 
 
 class SecureMessage:
@@ -73,25 +74,19 @@ class SecureMessage:
     def key_exchange(self):
         """TODO: Diffie-Hellman key exchange"""
 
-        #server public key exchange
-        #server = pyDH.DiffieHellman()
-
-        #server_pubkey = server.gen_public_key()
-
-     
-        port = self.s.getsockname()
+        #port = self.s.getsockname()
      
         #server public key exchange
-        print("this is host:")
-        print(port[0])
-        print("this is port:")
-        print(port[1])
-        server = pyDH.DiffieHellman()
+        # print("this is host:")
+        # print(port[0])
+        # print("this is port:")
+        # print(port[1])
+        # server = pyDH.DiffieHellman()
 
-        server_pubkey = server.gen_public_key()
+        # server_pubkey = server.gen_public_key()
 
 
-        print("server key:")
+        """ print("server key:")
         print(server_pubkey)
         server_1 = "This is the servers public key:\n"
         self.s.sendall(str(server_1).encode('utf8'))
@@ -110,16 +105,40 @@ class SecureMessage:
         if(check == "Yes"):
             print("yes it is equal")
         else:
-            print("no")
-        #client public key 
+            print("no") """
+      
+
         client = pyDH.DiffieHellman()
+        server = pyDH.DiffieHellman()
+
+        # make the public keys for client and server
+
         client_pubkey = client.gen_public_key()
-     
-        
-        
+        server_pubkey = server.gen_public_key()
+
+        client_pubkey = (str(client_pubkey)+"\n").encode()
+        server_pubkey = (str(server_pubkey)+"\n").encode()
+
+        #send public keys and generate shared keys 
+
+        self.s.send(server_pubkey[:SEND_BUFFER_SIZE])
+        server_sentkey = int(self.s.recv(SEND_BUFFER_SIZE).decode())
+
+        self.server_sharedkey = server.gen_shared_key(server_sentkey)
+        self.s.send(client[:SEND_BUFFER_SIZE])
+
+        client_sentkey = int(self.s.recv(SEND_BUFFER_SIZE).decode())
+
+        self.client_sharedkey = client.gen_shared_key(client_sentkey)
+
+        #we will be using AES later and AES only takes keys max 32 bits 
+        self.client_sharedkey = self.str(client_sharedkey[:AES_KEY_SIZE])
        
+        self.client_key = str.encode(self.client_sharedkey)
+        self.server_key = str.encode(self.server_sharedkey)
         
         """
+        d1 server d2 client 
         d1 = pyDH.DiffieHellman()
         d2 = pyDH.DiffieHellman()
         d1_pubkey = d1.gen_public_key()
