75709fe764f62cdf234ebfd5f9286754d813ef16
ivargaschavez <63480312+ivargaschavez@users.noreply.github.com>
2021-09-16 10:46:06 -0400

    Getting used to it

--- SecureMessaging.py
+++ SecureMessaging.py
@@ -21,25 +21,28 @@ SEND_BUFFER_SIZE = 2048
 AES_KEY_SIZE = 32
 
 
-class SecureMessage:
 
+class SecureMessage:
+    
     def __init__(self, server_ip=None, server_port=None):
         """Initialize SecureMessage object, create & connect socket,
            do key exchange, and start send & receive loops"""
-    
+        
         # create IPv4 TCP socket
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         # connect as client
         if server_ip and server_port:
             self.s.connect((server_ip, server_port))
+            self.name = "client"
 
         # connect as server
         elif server_port and not server_ip:
             self.s.bind(('', server_port))
             self.s.listen(QUEUE_LENGTH)
             self.s, _ = self.s.accept()
-
+            self.name = "server"
+        
         # Run Diffie-Hellman key exchange
         self.key_exchange()
 
@@ -70,56 +73,45 @@ class SecureMessage:
                 sys.stdout.flush()
             else:
                 os._exit(0)
+    def mysend(self, msg):
+        totalsent = 0
+        while totalsent < len(msg):
+            sent = self.s.send(msg[totalsent:])
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            totalsent = totalsent + sent
 
     def key_exchange(self):
         """TODO: Diffie-Hellman key exchange"""
 
-        #port = self.s.getsockname()
+        port = self.s.getsockname()
      
-        #server public key exchange
-        # print("this is host:")
-        # print(port[0])
-        # print("this is port:")
-        # print(port[1])
-        # server = pyDH.DiffieHellman()
-
-        # server_pubkey = server.gen_public_key()
-
-
-        """ print("server key:")
-        print(server_pubkey)
-        server_1 = "This is the servers public key:\n"
-        self.s.sendall(str(server_1).encode('utf8'))
-
-        #need to send this key to client: python3 SecureMessaging.py [Server IP] [Server Port]
-        self.s.sendall(str(server_pubkey).encode('utf8'))
-        print("server public key sent to client")
-        msg = "Do you have a public key? (Yes / No)"
-        self.s.sendall(str(msg).encode('utf8'))
-        data = self.s.recv(SEND_BUFFER_SIZE)
-        print(data)
-        print("did we get it")
-        #bytes to str
-        check = data.decode('utf8')
-
-        if(check == "Yes"):
-            print("yes it is equal")
-        else:
-            print("no") """
       
+        print(port[0])
+        
+        print(port[1])
+        print(self.name)
+        # make the public keys for client and server and send
 
-        client = pyDH.DiffieHellman()
-        server = pyDH.DiffieHellman()
-
-        # make the public keys for client and server
-
-        client_pubkey = client.gen_public_key()
-        server_pubkey = server.gen_public_key()
-
-        client_pubkey = (str(client_pubkey)+"\n").encode()
-        server_pubkey = (str(server_pubkey)+"\n").encode()
-
-        #send public keys and generate shared keys 
+        #server
+        
+        if(self.name == "server"):
+            server = pyDH.DiffieHellman()
+            server_pubkey = server.gen_public_key()
+            server_pubkey = (str(server_pubkey)+"\n").encode()
+            self.mysend(b'server public key:')
+            self.mysend(server_pubkey)
+            
+            
+        #client
+        else:
+       
+            client = pyDH.DiffieHellman()
+            client_pubkey = client.gen_public_key()
+            client_pubkey = (str(client_pubkey)+"\n").encode()
+            self.mysend(b'client public key:')
+            self.mysend(client_pubkey)
+        """
 
         self.s.send(server_pubkey[:SEND_BUFFER_SIZE])
         server_sentkey = int(self.s.recv(SEND_BUFFER_SIZE).decode())
@@ -136,22 +128,15 @@ class SecureMessage:
        
         self.client_key = str.encode(self.client_sharedkey)
         self.server_key = str.encode(self.server_sharedkey)
-        
-        """
-        d1 server d2 client 
-        d1 = pyDH.DiffieHellman()
-        d2 = pyDH.DiffieHellman()
-        d1_pubkey = d1.gen_public_key()
-        d2_pubkey = d2.gen_public_key()
-        d1_sharedkey = d1.gen_shared_key(d2_pubkey)
-        d2_sharedkey = d2.gen_shared_key(d1_pubkey)
+        """    
+        #generate shared keys 
 
-        print(d1_sharedkey == d2_sharedkey)
-
-        print(d1_pubkey,'\n',d2_pubkey)
-        print(d1_sharedkey,' and ',d2_sharedkey)
-        print("hello world")
-        """
+        #self.s.send(str(server_pubkey).encode('utf8'))
+        #self.mysend(client_pubkey)
+        #server_sentkey = self.s.recv(SEND_BUFFER_SIZE).decode()
+        #print("sent key")
+        #print(server_sentkey)
+              
         pass
 
     def process_user_input(self, user_input):
@@ -194,3 +179,17 @@ if __name__ == "__main__":
     main()
 
    
+    """ print("server key:")
+    print(server_pubkey)
+    server_1 = "This is the servers public key:\n"
+    self.s.sendall(str(server_1).encode('utf8'))
+
+    #need to send this key to client: python3 SecureMessaging.py [Server IP] [Server Port]        self.s.sendall(str(server_pubkey).encode('utf8'))
+    print("server public key sent to client")
+    msg = "Do you have a public key? (Yes / No)"
+    self.s.sendall(str(msg).encode('utf8'))
+    data = self.s.recv(SEND_BUFFER_SIZE)
+    print(data)
+    print("did we get it")
+    #bytes to str
+    """
